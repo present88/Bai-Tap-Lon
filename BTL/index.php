c67713d7e94b1d3c7a54734c47b7fc0224beae86
present88 <1851061854@e.tlu.edu.vn>
2021-01-12 22:11:47 +0700

    up BTL

---
+++ BTL/index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/all.js"></script>
+    <script src="js/script.js"></script>
+    <header>
+        <div class="profile-page sidebar-collapse">
+            <nav id="navpage" class="navbar navbar-expand-lg fixed-top navbar-transparent" color-on-scroll="400">
+                <!-- <div class="container">
+                    <div class="navbar-translate "><a class="navbar-brand text-light" href="#" rel="tooltip" data-original-title="" title="">My CV</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-bar bar1"></span><span class="navbar-toggler-bar bar2"></span><span class="navbar-toggler-bar bar3"></span></button>
+                    </div>
+                    <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link text-light" href="#about">About</a></li>
+                            <li class="nav-item"><a class="nav-link text-light" href="#skill">Skills</a></li>
+                            <li class="nav-item"><a class="nav-link text-light" href="#portfolio">Portfolio</a></li>
+                            <li class="nav-item"><a class="nav-link text-light" href="#experience">Experience</a></li>
+                            <li class="nav-item"><a class="nav-link text-light" href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div> -->
+                <div class="container">
+                    <div class="navbar-translate container-fluid"><a class="navbar-brand text-light" href="#" rel="tooltip">Creative CV</a>
+                        <button class="navbar-toggler bg-light" style="float: right;" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars"></i>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse justify-content-end" align="right" id="navigation">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link smooth-scroll text-light" href="#about">About</a></li>
+                            <li class="nav-item"><a class="nav-link smooth-scroll text-light" href="#skill">Skills</a></li>
+                            <li class="nav-item"><a class="nav-link smooth-scroll text-light" href="#portfolio">Portfolio</a></li>
+                            <li class="nav-item"><a class="nav-link smooth-scroll text-light" href="#experience">Experience</a></li>
+                            <li class="nav-item"><a class="nav-link smooth-scroll text-light" href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        </nav>
+    </header>
+    <div class="page-content">
+        <div class="bg-img container-fluid">
+            <div class="avatar">
+                <img class="rounded-pill mb-4" src="image/img.jpg" alt="" width="180px" height="180px">
+                <h2>My name is Phương</h2>
+            </div>
+            <div class="section">
+                <div class="container">
+                    <div class="button-container" align="center">
+                        <a class="btn rounded-circle btn-lg btn-icon ml-1 mr-1" href="#" rel="tooltip" title="Follow me on Facebook"><i class="fab fa-facebook"></i></a>
+                        <a class="btn rounded-circle btn-lg btn-icon ml-1 mr-1" href="#" rel="tooltip" title="Follow me on Twitter"><i class="fab fa-twitter"></i></a>
+                        <a class="btn rounded-circle btn-lg btn-icon ml-1 mr-1" href="#" rel="tooltip" title="Follow me on Google+"><i class="fab fa-google-plus"></i></a>
+                        <a class="btn rounded-circle btn-lg btn-icon ml-1 mr-1" href="#" rel="tooltip" title="Follow me on Instagram"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
